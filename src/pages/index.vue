7ed5079994f4a4f04d91f1ebb06801af03042e7d
Mind-thatsall <mathieurossi77@gmail.com>
2023-05-29 23:04:06 +0200

    Init

---
+++ src/pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { invoke } from "@tauri-apps/api";
+
+const ping = async () => {
+  try {
+    const res = await invoke("get_random_data");
+    console.log(res);
+  } catch (e) {
+    console.error("error:", e);
+  }
+};
+
+</script>
+
+<template>
+  <h1>Hello World !</h1>
+  <button @click="ping">ping</button>
+
+  <NuxtLink to="/about">Go to About</NuxtLink>
+</template>
