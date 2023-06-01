041c6d7d497458d30924d487ac13c09ca02e3afe
Mind-thatsall <mathieurossi77@gmail.com>
2023-06-01 09:18:14 +0200

    added: create scenes

--- src/pages/chapters/[chapterId]/routes/[id].vue
+++ src/pages/chapters/[chapterId]/routes/[id].vue
@@ -1,1 +1,7 @@
-<template>specific route</template>
+<template>
+  <h1 class="text-7xl pl-[50px] pt-[50px] font-extrabold">Routes connected to</h1>
+  <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 p-[50px] gap-6">
+    <RouteCard sceneId="1" />
+    <RouteCard sceneId="2"/>
+  </div>
+</template>
